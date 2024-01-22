5a736bea6b261a157c59605627aede4edc2a995f
s4shebi <s4shebi75@gmail.com>
2024-01-22 13:01:55 +0500

    Analog clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return ()=> clearInterval(interval);
+  }, []);
+
+  const getRotation = (unit, value, max) => (360 / max) * (value % max) + unit;
+
+  const secondsRotation = getRotation(90, time.getSeconds(), 60);
+  const minutesRotation = getRotation(90, time.getMinutes(), 60);
+  const hoursRotation = getRotation(90, time.getHours(), 12);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="clock">
+        <div className="hand seconds1" style={{ transform: `rotate(${secondsRotation}deg)`}}></div>
+        <div className="hand minutes" style={{ transform: `rotate(${minutesRotation}deg)`}}></div>
+        <div className="hand hours" style={{ transform: `rotate(${hoursRotation}deg)`}}></div>
+      </div>
     </div>
   );
 }
